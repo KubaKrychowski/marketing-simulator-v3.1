f1798a2b49eedd2ecc1ecf448725efd19df0794b
Kuba Krychowski <kuba.krychowski@mieszkanie.pl>
2022-06-16 08:02:13 +0200

    Added map

--- src/app/feature/create-user-profile/create-user-profile.component.ts
+++ src/app/feature/create-user-profile/create-user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Loader } from "@googlemaps/js-api-loader";
 
 @Component({
   selector: 'app-create-user-profile',
@@ -12,7 +13,20 @@ export class CreateUserProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: "AIzaSyCYqXihdiQaWiw7BIgvpdu4seFMGGJz_Qw",
+      version: "weekly"
+    })
 
+    loader.load().then(() => {
+      new google.maps.Map(document.getElementById('map'),{
+        center: {
+          lat:  52.237049,
+          lng: 21.017532
+        },
+        zoom: 6
+      });
+    });
   }
 
   ngAfterViewInit(): void {
